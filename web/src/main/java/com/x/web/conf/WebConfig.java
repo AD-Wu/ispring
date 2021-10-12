021473d18c1ec4cb5c82293cada010d5c50270e0
AD-Wu <944907103@qq.com>
2021-10-12 13:33:47 +0800

    -

--- web/src/main/java/com/x/web/conf/WebConfig.java
+++ web/src/main/java/com/x/web/conf/WebConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    
+
     /**
      * 跨域配置会覆盖默认的配置，
      * 因此需要实现addResourceHandlers方法，增加默认配置静态路径
@@ -27,9 +27,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/resources/")
                 .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/static/api/")
                 .addResourceLocations("classpath:/doc/");
     }
-    
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -38,5 +39,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
                 .maxAge(3600 * 24);
     }
-    
+
 }
