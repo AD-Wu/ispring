8765dd9f126c3027e8080d16f1a15e33acae4f88
AD-Wu <944907103@qq.com>
2021-10-12 15:09:00 +0800

    -

--- Product-Service/src/main/java/com/x/ProductApp.java
+++ Product-Service/src/main/java/com/x/ProductApp.java
@@ -1,10 +1,22 @@
 package com.x;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * TODO
  *
  * @author AD
  * @date 2021/10/10 23:08
  */
+@SpringBootApplication
 public class ProductApp {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(ProductApp.class,args);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
