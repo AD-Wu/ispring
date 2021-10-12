bf3b169eafd69711b9c42e93a0bba032bc4696fc
AD-Wu <944907103@qq.com>
2021-10-12 15:09:20 +0800

    -

---
+++ Product-Service/src/main/java/com/x/product/init/OpenBrowse.java
@@ -0,0 +1,26 @@
+package com.x.product.init;
+
+import com.x.Starter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * 启动类
+ *
+ * @author AD
+ * @date 2021/10/8 21:29
+ */
+@Log4j2
+@Component
+public class OpenBrowse extends Starter {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Override
+    public String[] getBrowseUrl() {
+        return new String[]{"http://localhost:" + port + "/api/index.html"};
+    }
+
+}
